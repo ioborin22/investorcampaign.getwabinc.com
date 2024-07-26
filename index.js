bfdaa4af45889a817e5e1d83c641e76c8d6a10c0
ioborin22 <ioborin22@gmail.com>
2024-07-26 16:24:08 -0400

    Updated web design philosophy and call-to-action text

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Устанавливаем директорию для статических файлов
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Отправка главной страницы
 app.get('/', (req, res) => {
-  res.send('Hello, welcome to empstateweb!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
