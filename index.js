dea7505dcc6d72b83eed901c71e239eb4dd99a69
ioborin22 <ioborin22@gmail.com>
2024-11-13 14:32:30 -0500

    20241113

--- index.js
+++ index.js
@@ -10,36 +10,26 @@ app.set('views', path.join(__dirname, 'views'));
 // Устанавливаем директорию для статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Отправка главной страницы
-app.get('/', (req, res) => {
-  res.render('index');
+// Определенные маршруты
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/services', (req, res) => res.render('services'));
+app.get('/pricing', (req, res) => res.render('pricing'));
+app.get('/portfolio', (req, res) => res.render('portfolio'));
+app.get('/contact', (req, res) => res.render('contact'));
+app.get('/blog', (req, res) => res.render('blog'));
+app.get('/manifest.json', (req, res) =>
+  res.sendFile(path.join(__dirname, 'manifest.json')),
+);
+
+// Маршрут для страницы 404
+app.get('/404', (req, res) => {
+  res.status(404).render('404');
 });
 
-// Отправка страницы About
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-// Отправка страницы Services
-app.get('/services', (req, res) => {
-  res.render('services');
-});
-
-// Другие маршруты для остальных страниц
-app.get('/pricing', (req, res) => {
-  res.render('pricing');
-});
-
-app.get('/portfolio', (req, res) => {
-  res.render('portfolio');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.get('/manifest.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'manifest.json'));
+// Перенаправление всех несуществующих маршрутов на страницу 404
+app.use((req, res) => {
+  res.redirect('/404');
 });
 
 app.listen(port, () => {
