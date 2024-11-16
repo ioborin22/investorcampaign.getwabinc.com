013b97676c19490658d2af7db0220e0d1e23a755
ioborin22 <ioborin22@gmail.com>
2024-11-16 14:51:32 -0500

    20241116

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ app.get('/services', (req, res) => res.render('services'));
 app.get('/pricing', (req, res) => res.render('pricing'));
 app.get('/portfolio', (req, res) => res.render('portfolio'));
 app.get('/contact', (req, res) => res.render('contact'));
+app.get('/process', (req, res) => res.render('process'));
+app.get('/guarantee', (req, res) => res.render('guarantee'));
+app.get('/legal-info', (req, res) => res.render('legal-info'));
+app.get('/web-design', (req, res) => res.render('web-design'));
+app.get('/web-app-development', (req, res) =>
+  res.render('web-app-development'),
+);
+app.get('/seo', (req, res) => res.render('seo'));
+app.get('/hosting', (req, res) => res.render('hosting'));
 app.get('/blog', (req, res) => res.render('blog'));
+app.get('/faq', (req, res) => res.render('faq'));
+app.get('/support', (req, res) => res.render('support'));
+app.get('/location', (req, res) => res.render('location'));
+app.get('/careers', (req, res) => res.render('careers'));
 app.get('/manifest.json', (req, res) =>
   res.sendFile(path.join(__dirname, 'manifest.json')),
 );
