868a4c221646a2a72c311e886b8370588d74d00a
ioborin22 <ioborin22@gmail.com>
2024-12-04 18:13:12 -0500

    20241204

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     burgerMenu.classList.toggle('active');
     headerMobile.classList.toggle('active');
 
-    // Добавляем или убираем класс body-without-scroll для блокировки прокрутки основного контента
     if (headerMobile.classList.contains('active')) {
       document.body.classList.add('body-without-scroll');
     } else {
